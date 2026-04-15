75a5a476a4758877a81708dcc84550ab6a5a78bd
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-15 10:41:16 +0530

    Recursion

---
+++ Day6/recNum.java
@@ -0,0 +1,16 @@
+package Day6;
+
+public class recNum {
+    
+    static void num(int n){
+        if (n==0) {
+            return;
+        }
+        num(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        num(5);
+    }
+
+}
