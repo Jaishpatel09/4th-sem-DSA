e901cb54982441fec29c7f24b92729fb241e5373
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-22 11:20:34 +0530

    Loops in Array

---
+++ Day7/LoopsArray.java
@@ -0,0 +1,8 @@
+public class LoopsArray {
+    public static void main(String[] args) {
+      String arr[]={"Hello","Hii..","Good Morning","Good Evening","Good Night"};
+      for (int i = 0; i < arr.length; i++) {
+        System.out.println(arr[i]);
+      }
+    }
+}
