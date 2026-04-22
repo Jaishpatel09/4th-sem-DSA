9f81c731f945e9160c3fed77160c61c0c44a27ba
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-22 12:50:12 +0530

    Arr

---
+++ Day7/seatth.java
@@ -0,0 +1,9 @@
+public class seatth {
+    public static void main(String[] args) {
+        String[] seats = {"Jenny", "Liam", "Angie", "Boko"};
+        for (int i = 0; i < seats.length; i++) {
+            System.out.println("Seat no. "+i+" is obtained by "+seats[i]);
+        }
+
+    }
+}
