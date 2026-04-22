75654e57006f8becb365f38e101b29718417f481
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-22 11:09:48 +0530

    Specifying Size Of An Array

---
+++ Day7/SpecifyingSizeOfArray.java
@@ -0,0 +1,15 @@
+public class SpecifyingSizeOfArray {
+    public static void main(String[] args) {
+        // String arr[]=new String[5];//Array Stores 5 Elements
+        // arr[0]="Hello";
+        // arr[1]="Hii..";
+        // arr[2]="Good Morning";
+        // arr[3]="Good Evening";
+        // arr[4]="Good Night";
+        // System.out.println(arr[3]);
+    //To Store Element Values in same line most common
+    String arr[]={"Hello","Hii..","Good Morning","Good Evening","Good Night"};
+    System.out.println(arr[3]);   
+    System.out.println(arr[2]); 
+    }
+}
