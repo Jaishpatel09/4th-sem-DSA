1c34f755b70dfafd77fd30cedb1d190084932ece
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-22 11:41:15 +0530

    For Eachh

---
+++ Day7/ForEachElmnt.java
@@ -0,0 +1,8 @@
+public class ForEachElmnt {
+    public static void main(String[] args) {
+        String greet[]={"Hello","Hii..","Good Morning","Good Evening","Good Night"};
+            for (String greets: greet) {  //Print all the element without using loop
+            System.out.println(greets);
+        }
+    }
+}
