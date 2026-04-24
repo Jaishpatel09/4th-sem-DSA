a045cf24a90ea81edb753d98e70cade3879e79ce
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-24 11:29:43 +0530

    Dp Recursion

---
+++ Day8/ReverseDP.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ReverseDP {
+
+    static String ways(String str, String[] dp) {
+        if (str.length() == 0) return "";
+
+        int len = str.length();
+
+        if (dp[len] != null) return dp[len];
+
+        dp[len] = ways(str.substring(1), dp) + str.charAt(0);
+
+        return dp[len];
+    }
+
+    public static void main(String[] args) {
+        String str = "hello";
+        String[] dp = new String[str.length() + 1];
+
+        System.out.println(ways(str, dp));
+    }
+}
---
+++ Day8/count.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class count {
+    static  int ways(int n, int[] dp)
+{
+      if (n==0) return 0;
+      if (dp[n]!=-1) {
+        return dp[n];
+      }
+      dp[n]=1+ways(n/10, dp);
+      return dp[n];
+}
+    public static void main(String[] args) {
+        int n = 123456;
+        int []dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        System.out.println(ways(n, dp));
+    }
+}
